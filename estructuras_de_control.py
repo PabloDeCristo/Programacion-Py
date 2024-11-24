a0464068d78b2511e61d6a9bb60c27335d7f567f
PabloDeCristo <hpablocavieres@gmail.com>
2024-11-24 19:19:29 -0300

    Add files via upload

---
+++ estructuras_de_control.py
@@ -0,0 +1,18 @@
+edad=19
+
+if edad < 18: 
+    print("Eres menor de edad.")
+
+elif edad >= 18 and edad < 60:
+    print("Eres un adulto.")
+
+elif edad > 15:
+    print("Mayor a 15 años.")
+
+elif edad == 60:
+    print("Feliz Cumpleaños.")
+
+else:
+    print("Eres adulto mayor.")        
+
+    #se puede usar este mismo metodo para crear varias herramientas como el juego del tictactoe o conocido como gato
